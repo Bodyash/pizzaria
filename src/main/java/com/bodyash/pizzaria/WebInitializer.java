b85229f5e909994c5a3ddf88f2c83da0b3739bb4
Bogdan Shkliarenko <bogdanhalfist93@gmail.com>
2017-06-09 17:49:55 +0300

    Cleanup WebInit

--- src/main/java/com/bodyash/pizzaria/WebInitializer.java
+++ src/main/java/com/bodyash/pizzaria/WebInitializer.java
@@ -29,23 +29,14 @@ public class WebInitializer extends
     
     @Override
     protected Filter[] getServletFilters() {
-
     	   // if encoding has issues we need to add UTF-8 encoding filter
-
     	   CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter();
-
     	   encodingFilter.setForceEncoding(true);
-
     	   encodingFilter.setEncoding("UTF-8");
-
     	   // encoding filter must be the first one
-
     	   return new Filter[]{encodingFilter,
-
     	           new DelegatingFilterProxy("springSecurityFilterChain"),
-
     	           new OpenEntityManagerInViewFilter()};
-
     	}
     
 
